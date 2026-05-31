468fd03c65e3fbf659397ec809a45cf227531a34
Joevert Bentulan <joevertbentulan@192.168.1.87>
2026-05-31 21:27:10 +0800

    update section title

--- sections.php
+++ sections.php
@@ -65,7 +65,7 @@
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
    
-    <title>Sections</title>
+    <title>Sections Online</title>
 </head>
 <body>
     
